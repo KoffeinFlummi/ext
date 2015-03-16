af0c49ed4a344bc748d12fa9697fa9d6f98c5007
KoffeinFlummi <koffeinflummi@gmail.com>
2015-03-16 13:28:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+ext
+===
+
+[![License](http://img.shields.io/badge/license-MIT-red.svg)](https://github.com/KoffeinFlummi/ext/blob/master/LICENSE)
+
+Extract archives without having to worry about different `tar` parameters, or whether you should use a subdirectory or not.
+
+ext extracts files to your CWD if there's only one directory or file in there, and to a subfolder if there's more.
+
+
+### Setup
+
+```
+$ python3 setup.py install
+```
+
+
+### Usage
+
+```
+$ ext [pathtoarchive]
+```
+
+Supports rars, zips and tarballs.
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import platform
+from setuptools import setup, find_packages
+
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = "ext",
+  version = "1.0",
+  packages = [],
+  scripts = ["scripts/ext"],
+  install_requires = [],
+  author = "Felix \"KoffeinFlummi\" Wiegand",
+  author_email = "koffeinflummi@gmail.com",
+  description = "Extract things without having to worry about parameters.",
+  long_description = read("README.md"),
+  license = "MIT",
+  keywords = "extract archives archive utility",
+  url = "https://github.com/KoffeinFlummi/ext",
+  classifiers=[
+    "Environment :: Console",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Topic :: Utilities"
+  ]
+)
